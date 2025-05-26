f1bf683db1974775f410d0477402a116a7e73f5b
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2025-05-26 21:10:52 +0530

    added requests page

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addRequest } from '../utils/requestSlice';
 
 const Requests = () => {
+
+  const dispatch = useDispatch();
+
+
+  const fetchRequests=async()=>{
+    try {
+      const res=await axios.get(BASE_URL+"/user/requests/recieved",{
+        withCredentials:true
+      })
+      console.log(res.data);
+
+      dispatch(addRequest(res.data));
+    } catch (error) {
+      console.error("Error fetching requests:", error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchRequests();
+  },[])
   return (
     <div>Requests</div>
   )
